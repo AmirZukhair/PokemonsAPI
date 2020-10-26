5346eacae35a789cb73b46e8ebb207a10d911800
Amir_Zukhair <AmirZukhair@gmail.com>
2020-10-27 01:06:21 +0300

    Updates

---
+++ precache-manifest.ca2e0ecf45994a12bed66341736cf46b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1f8f0e6d229663954bdb60c3df51223",
+    "url": "/AmirZukhair/PokemonsAPI/index.html"
+  },
+  {
+    "revision": "bbca845c464aa2ecf827",
+    "url": "/AmirZukhair/PokemonsAPI/static/css/main.df4d8cdd.chunk.css"
+  },
+  {
+    "revision": "5d328d3aa371ff2df14a",
+    "url": "/AmirZukhair/PokemonsAPI/static/js/2.2d63c8b4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/AmirZukhair/PokemonsAPI/static/js/2.2d63c8b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbca845c464aa2ecf827",
+    "url": "/AmirZukhair/PokemonsAPI/static/js/main.b35a1d64.chunk.js"
+  },
+  {
+    "revision": "738d73f9b01c37857bc8",
+    "url": "/AmirZukhair/PokemonsAPI/static/js/runtime-main.d78e04fd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/AmirZukhair/PokemonsAPI/static/media/logo.5d5d9eef.svg"
+  }
+]);
